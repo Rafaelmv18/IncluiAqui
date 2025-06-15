9da7437fcc4d052848f9fa6eecd8cc4ce4db0e1b
Rafael Mascarenhas Venas <rafavenas@gmail.com>
2025-06-15 20:17:32 -0300

    button

--- IncluiAqui/src/components/button/index.tsx
+++ IncluiAqui/src/components/button/index.tsx
@@ -1,17 +1,26 @@
 import { TouchableOpacity, Text, ViewStyle, TextStyle, TouchableOpacityProps } from "react-native";
 import { styles } from "./styles"; // Supondo que os estilos estejam em styles.js
+import { Feather } from "@expo/vector-icons";
 
 type Props = TouchableOpacityProps & {
-  title: string;
+  icon?: keyof typeof Feather.glyphMap;
+  title?: string;
   style?: ViewStyle;
   titleStyle?: TextStyle;
   pages?: string;
+  iconStyle?: object; 
 };
 
-export function Button({ title, style, titleStyle, pages, ...rest }: Props) {
+export function Button({ title, style, titleStyle, pages, iconStyle, icon, ...rest }: Props) {
   return (
     <TouchableOpacity activeOpacity={0.8} style={[styles.button, style]} {...rest}>
       {/* PONTO CRÍTICO: titleStyle DEVE VIR POR ÚLTIMO */}
+      <Feather  
+        name={icon}
+        size={24}
+        color={"#DB6300"}
+        style={iconStyle}
+      />
       <Text style={[styles.title, titleStyle]}>{title}</Text> 
       
     </TouchableOpacity>
