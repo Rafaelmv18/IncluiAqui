4f4230c69e14ff7b9081ab4dfa4173d6491c494a
Rafael Mascarenhas Venas <rafavenas@gmail.com>
2025-06-16 10:15:30 -0300

    icon button

--- IncluiAqui/src/components/iconButton/index.tsx
+++ IncluiAqui/src/components/iconButton/index.tsx
@@ -1,18 +1,29 @@
 // src/components/IconButton.tsx
 import { TouchableOpacity, TouchableOpacityProps } from "react-native";
 import { Feather } from "@expo/vector-icons";
-import { styles } from "./styles"; // certifique-se de que o styles.ts esteja no mesmo local
+import { styles } from "./styles";
 
 type Props = TouchableOpacityProps & {
   icon: keyof typeof Feather.glyphMap;
   iconColor?: string;
   iconSize?: number;
+  isSelected?: boolean; // <- aqui!
 };
 
-export function IconButton({ icon, iconColor = "#DB6300", iconSize = 30, ...rest }: Props) {
+export function IconButton({
+  icon,
+  iconColor = "#2A2A2A",
+  iconSize = 30,
+  isSelected = false, // <- aqui!
+  ...rest
+}: Props) {
   return (
     <TouchableOpacity style={styles.iconButton} activeOpacity={0.8} {...rest}>
-      <Feather name={icon} size={iconSize} color={iconColor} />
+      <Feather
+        name={icon}
+        size={iconSize}
+        color={isSelected ? "#DB6300" : iconColor} // <- muda a cor se estiver selecionado
+      />
     </TouchableOpacity>
   );
 }
