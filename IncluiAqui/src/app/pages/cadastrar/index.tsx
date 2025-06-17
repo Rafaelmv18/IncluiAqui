8727f466d44dbfc76cb5885b2e32a76be212be87
Rafael Mascarenhas Venas <rafavenas@gmail.com>
2025-06-17 11:40:39 -0300

    cadastrar

--- IncluiAqui/src/app/pages/cadastrar/index.tsx
+++ IncluiAqui/src/app/pages/cadastrar/index.tsx
@@ -1,26 +1,59 @@
-import { View, Text, StyleSheet } from "react-native"
-import { router } from "expo-router"
+import { useState } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import { router } from "expo-router";
 
+import { styles } from "./styles";
+import { Input } from "@/src/components/input";
 import { Button } from "@/src/components/button";
+import { Feather } from "@expo/vector-icons";
 
-export default function Dashboard(){
-    return(
-        <View style={styles.container}>
-            <Text style={styles.title}>Dashboard</Text>
-            <Button title="Voltar" onPress={() => router.back()}></Button>
+export default function Index() {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const pages = useState("");;
+
+  function handleNext(pages: string){
+        router.navigate(`../pages/${pages}`)
+    }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.avatar}>
+        <Feather name="user" size={48} color="#fff" />
+      </View>
+      {/* Tem que alinhar o card no centro da tela */}
+      <View style={styles.card}>
+        <Input
+          placeholder="Email"
+          value={email}
+          onChangeText={setEmail}
+          icon="mail"
+        />
+        <Input
+          placeholder="Usuário"
+          value={name}
+          onChangeText={setName}
+          icon="user"
+        />
+        <Input
+          placeholder="Senha"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+          icon="lock"
+        />
+        
+        <View style={styles.buttonRow}>
+          <Button
+            title="Cadastrar"
+            style={styles.buttonFilled}
+            onPress={() => handleNext("login")}
+          />
         </View>
-    )
-}
+      </View>
+    </View>
 
-export const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        padding: 42
-    },
-    title:{
-        fontSize: 18,
-        fontWeight: "bold",
-    },
-})
+
+  );
+}
---
+++ IncluiAqui/src/app/pages/cadastrar/styles.ts
@@ -0,0 +1,56 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#f2f2f2",
+    },
+    avatar:{
+        width: 80,
+        height: 80,
+        borderRadius: "50%",
+        backgroundColor: "#DB6300",
+        alignItems: "center",
+        justifyContent: "center",
+        marginBottom: 20,
+        elevation: 8,
+
+        // sombra iOS
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+    },
+    card: {
+        alignItems: "center",
+        width: 300,
+        padding: 24,
+        borderRadius: 15,
+        backgroundColor: "#fff", 
+        gap: 10,
+
+        // sombra iOS
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+
+        // sombra Android
+        elevation: 8,
+        
+    },
+    buttonRow: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        gap: 12, // ou paddingHorizontal
+    },
+
+    buttonFilled: {
+        flex: 1,
+        width: 230,
+        backgroundColor: "#DB6300",
+    },
+
+});
