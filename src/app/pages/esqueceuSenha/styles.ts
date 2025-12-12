7f958433e9f22e01e5a6ae73d043710e4b194c47
Rafael Mascarenhas Venas <rafavenas@gmail.com>
2025-12-12 10:06:40 -0300

    style esqueceuSenha update

--- src/app/pages/esqueceuSenha/styles.ts
+++ src/app/pages/esqueceuSenha/styles.ts
@@ -1,47 +1,17 @@
 import { StyleSheet } from "react-native";
+import { theme } from "@/src/themes";
 
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "center",
+        justifyContent: "flex-start", // Alinha ao topo
         alignItems: "center",
-        backgroundColor: "#f2f2f2",
-        
-    },
-    title:{
-        fontSize: 30,
-        fontWeight: "bold",
-        margin: 10
-    },
-    card: {
-        alignItems: "center",
-        width: 300,
-        padding: 24,
-        borderRadius: 15,
-        backgroundColor: "#fff", 
-        gap: 10,
-
-        // sombra iOS
-        shadowColor: "#000",
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-
-        // sombra Android
-        elevation: 8,
-        
+        backgroundColor: theme.colors.background,
+        paddingTop: 32,
+        paddingBottom: 40,
     },
     buttonRow: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        gap: 12, // ou paddingHorizontal
-        margin: 10
+        width: "100%",
+        marginTop: 24, // Espaço maior antes do botão principal
     },
-
-    buttonFilled: {
-        flex: 1,
-        backgroundColor: "#DB6300",
-    },
-
-
 });
